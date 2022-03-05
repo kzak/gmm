cb6b8b4350728b4a2eb8bc9fff1cefca1c3e87c2
kzak <15907+kzak@users.noreply.github.com>
2022-03-05 16:00:09 +0900

    Implemented gibbs#gibbs_sampling 	modified:   infer/gibbs.py 	modified:   tests/test_gibbs.py

--- infer/gibbs.py
+++ infer/gibbs.py
@@ -1,6 +1,7 @@
 from pdb import set_trace
 
 import numpy as np
+from numpy.linalg import pinv
 from scipy import stats
 
 
@@ -75,6 +76,123 @@ def gibbs_init(X, K):
     smpl_dict["S"] = [np.array(S).reshape(len(X), -1)]
 
     return smpl_dict
+
+
+def draw_s_n(x_n, mu, Lmd, pi, eps=1.0e-5):
+    """
+    n: Int >= 0
+    """
+
+    eta_n = []
+    for k in range(len(mu)):
+        eta_nk = np.exp(
+            -0.5 * (x_n - mu[k]) @ Lmd[k] @ (x_n - mu[k]).T
+            + 0.5 * np.linalg.slogdet(Lmd[k])[1]
+            + np.log(pi[k])
+        )
+        eta_n += [eta_nk + eps]  # eps ; to avoid zero division
+
+    eta_n = np.array(eta_n) / sum(eta_n)
+
+    return np.random.multinomial(1, pvals=eta_n)
+
+
+def draw_Lmd_k_mu_k(X_k, S_k, m, beta, nu, W_inv):
+    """
+    Args
+        X: (N, D) matrix
+        S: (N, K) matrix
+    Returns
+        Lmd_k: (D, D) matrix
+    """
+
+    sum_s_k = S_k.sum()
+    beta_hat = sum_s_k + beta
+    m_hat_k = (X_k.sum(axis=0) + beta * m) / beta_hat
+    nu_hat = sum_s_k + nu
+
+    W_hat_k_inv = (
+        X_k.T @ X_k
+        + beta * np.outer(m, m)
+        - beta_hat * np.outer(m_hat_k, m_hat_k)
+        + W_inv
+    )
+
+    Lmd_k = stats.wishart(nu_hat, W_hat_k_inv).rvs()
+    mu_k = stats.multivariate_normal(m_hat_k, pinv(beta_hat * Lmd_k)).rvs()
+
+    return Lmd_k, mu_k
+
+
+def draw_pi(S, alpha):
+    """
+    Args
+        S: (N, K) matrix
+        alpha: K array
+    Returns
+        pi_new: K array
+    """
+    alpha_hat = np.sum(S.T, axis=1) + alpha
+    return stats.dirichlet.rvs(alpha_hat)[0]
+
+
+def gibbs_sampling(X, K, n_iter=100):
+    """
+    Args
+        X: (N, D) matrix
+        K: Int > 0
+        n_iter: Int > 0
+    Returns
+        smpl_dict: Dict
+            {
+                "pi": [[pi_1, ..., pi_K], ...],
+                "mu": [[mu_1, ..., mu_K], ...],
+                "Lmd": [[Lmd_1, ..., Lmd_K], ...],
+                "S": [[S], ...]
+            }
+    """
+    # Initialization
+    N, D = X.shape
+    ss = gibbs_init(X, K)
+
+    # Hyper parameters
+    alpha = np.ones(K)
+    beta = 1.0
+    nu = D
+    W_inv = np.array([[1, 0], [0, 1]])
+
+    # Gibbs sampling
+    for i in range(n_iter):
+
+        # Draw S
+        S = []
+        for n in range(N):
+            S += [draw_s_n(X[n], ss["mu"][-1], ss["Lmd"][-1], ss["pi"][-1])]
+        ss["S"] += [np.array(S).reshape(N, K)]
+
+        # Draw Lmd, mu
+        Lmd = []
+        mu = []
+        for k in range(K):
+            Lmd_k, mu_k = draw_Lmd_k_mu_k(
+                X[ss["S"][-1][:, k] == 1],  # X_k
+                ss["S"][-1][:, k],  # S_k
+                ss["mu"][0][k],  # m = mu_k
+                beta,
+                nu,
+                W_inv,
+            )
+            Lmd += [Lmd_k]
+            mu += [mu_k]
+
+        ss["Lmd"] += [Lmd]
+        ss["mu"] += [mu]
+
+        # Draw pi
+        pi = draw_pi(ss["S"][-1], alpha)
+        ss["pi"] += [pi]
+
+    return ss
 
 
 if __name__ == "__main__":
--- tests/test_gibbs.py
+++ tests/test_gibbs.py
@@ -1,5 +1,5 @@
 import numpy as np
-from infer.gibbs import gibbs_init, gmm_gen
+from infer.gibbs import gibbs_init, gibbs_sampling, gmm_gen
 from scipy import stats
 
 
@@ -31,3 +31,18 @@ def test_gibbs_init():
     assert X.ndim == len(ss["mu"][0])
     assert (X.ndim, X.ndim) == ss["Lmd"][0][0].shape
     assert (len(X), X.ndim) == ss["S"][0].shape
+
+
+def test_gibbs_sampling():
+    N = 100
+    D = 2
+    K = 2
+    alpha = [1, 1]
+    nu = 3
+    W = np.array([[1, 0], [0, 1]])
+    X = gmm_gen(N, D, K, alpha, nu, W)
+
+    n_iter = 10
+    ss = gibbs_sampling(X, K, n_iter)
+
+    assert (n_iter + 1) == len(ss["pi"])
